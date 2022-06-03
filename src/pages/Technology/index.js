eac80216374cefcef8e7adea8622e57cc41bc1a7
Vasileios1314 <vasilis.bobonis@hotmail.com>
2022-06-03 15:20:00 +0200

    technologyPage carousel

--- src/pages/Technology/index.js
+++ src/pages/Technology/index.js
@@ -1,14 +1,21 @@
 import { Grid } from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import TechnologyCard from "../../components/TechnologyCard";
 import { setTechnology } from "../../store/technology/actions";
 import { selectTechnology } from "../../store/technology/selectors";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, Keyboard } from "swiper";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
 
 export default function Technology() {
   const dispatch = useDispatch();
   const technology = useSelector(selectTechnology);
-
   useEffect(() => {
     dispatch(setTechnology());
     console.log("response", technology);
@@ -29,29 +36,62 @@ export default function Technology() {
         03 SPACE LAUNCH
       </h1>
       <div>
-        <Grid
-          container
-          alignItems="center"
-          style={{
-            alignItems: "center",
-            marginLeft: 200,
-            marginBottom: 40,
-          }}
-          spacing={{ xs: 2, md: 4 }}
-        >
-          {technology.map((technology) => {
-            return (
-              <Grid item xs={2} sm={4} md={3} key={technology.id}>
-                <TechnologyCard
-                  key={technology.id}
-                  id={technology.id}
-                  name={technology.name}
-                  imgUrl={technology.imgUrl}
-                  description={technology.description}
-                />
-              </Grid>
-            );
-          })}
+        <Grid container>
+          <Grid item container xs={12} justifyContent="center">
+            <Grid item xs={3}>
+              <Card sx={{ backgroundColor: "transparent" }}>
+                <Swiper
+                  modules={[Navigation, Pagination, Scrollbar, Keyboard]}
+                  spaceBetween={5}
+                  slidesPerView={1}
+                  navigation
+                  keyboard
+                  loop
+                  pagination={{ clickable: true }}
+                  scrollbar={{ draggable: true }}
+                >
+                  {technology.map((technology) => {
+                    return (
+                      <SwiperSlide key={technology.id}>
+                        <Typography
+                          gutterBottom
+                          variant="h3"
+                          component="div"
+                          style={{
+                            color: "white",
+                            fontFamily: "monospace",
+                            letterSpacing: ".1rem",
+                          }}
+                        >
+                          {technology.name}
+                        </Typography>
+                        <CardMedia
+                          sx={{ borderRadius: 5 }}
+                          component="img"
+                          height="600"
+                          alt="planet"
+                          src={technology.imgUrl}
+                        />
+
+                        <Typography
+                          gutterBottom
+                          variant="h6"
+                          component="div"
+                          style={{
+                            color: "white",
+                            fontFamily: "monospace",
+                            letterSpacing: ".1rem",
+                          }}
+                        >
+                          {technology.description}
+                        </Typography>
+                      </SwiperSlide>
+                    );
+                  })}
+                </Swiper>
+              </Card>
+            </Grid>
+          </Grid>
         </Grid>
       </div>
     </div>
